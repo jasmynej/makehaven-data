63cf4c9f8d00ae102af71f8bcdcd3bc6861289ec
Jasmyne Jean-Remy <jasmynej14@gmail.com>
2025-11-10 13:41:43 -0500

    events queries, get # of participants per event and participants by status

--- queries/event_particpants.sql
+++ queries/event_particpants.sql
@@ -1,8 +1,19 @@
-select external_identifier e_id,
+select cc.external_identifier as contact_id,
        display_name,
-       events.id,
+       events.id event_id,
        events.title,
+       status.label,
        fee_level
 from civicrm_contact cc
 inner join civicrm_participant cp on cc.external_identifier = cp.contact_id
-inner join civicrm_event events on cp.event_id = events.id;
+inner join civicrm_event events on cp.event_id = events.id
+left join civicrm_participant_status_type status on cp.status_id = status.id;
+
+
+select events.id as event_id, events.title, count(cp.contact_id) as num_participants, events.start_date
+from civicrm_participant cp
+left join civicrm_event events on cp.event_id = events.id
+    where cp.status_id = 1
+group by event_id
+order by num_participants DESC;
+
